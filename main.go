9d2bd82d1edae6c050bc9d9f4b3a9f71837e631b
is386 <is386@drexel.edu>
2022-01-29 13:16:32 -0500

    move test out of main

--- main.go
+++ main.go
@@ -1,13 +1,4 @@
 package main
 
-import "github.com/is386/Go8080/i8080Test"
-
-var (
-	FILENAME = "roms/TST8080.COM"
-	DEBUG    = false
-)
-
 func main() {
-	m := i8080Test.NewTestMachine(FILENAME, DEBUG)
-	m.Run()
 }
