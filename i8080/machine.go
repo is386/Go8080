d5344689800b1bfdfc42d3eccbdb47989b1566ba
is386 <is386@drexel.edu>
2022-01-28 20:33:41 -0500

    add invaders ports

--- i8080/machine.go
+++ i8080/machine.go
@@ -15,8 +15,8 @@ func NewInvadersMachine(filename string, pcStart uint16, showDebug bool, isTest 
 	return im
 }
 
-func (im *InvadersMachine) setEmu(e *CPU) {
-	im.cpu = e
+func (im *InvadersMachine) setEmu(c *CPU) {
+	im.cpu = c
 }
 
 func (im *InvadersMachine) Run() {
@@ -26,8 +26,36 @@ func (im *InvadersMachine) Run() {
 	}
 }
 
+func (im *InvadersMachine) fetchNext() uint8 {
+	return im.cpu.mem[im.cpu.pc+1]
+}
+
+func (im *InvadersMachine) setAccumulator(val uint8) {
+	im.cpu.reg.A = val
+}
+
+func (im *InvadersMachine) getAccumulator() uint8 {
+	return im.cpu.reg.A
+}
+
 func (im *InvadersMachine) PortIn() {
+	port := im.fetchNext()
+	switch port {
+	case 3:
+		v16 := (uint16(im.shift1) << 8) | uint16(im.shift0)
+		v8 := uint8((v16 >> (8 - im.offset)) & 0xff)
+		im.setAccumulator(v8)
+	}
 }
 
 func (im *InvadersMachine) PortOut() {
+	a := im.getAccumulator()
+	port := im.fetchNext()
+	switch port {
+	case 2:
+		im.offset = a & 0x7
+	case 4:
+		im.shift0 = im.shift1
+		im.shift1 = a
+	}
 }
