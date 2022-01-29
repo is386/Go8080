60904b46e980d5f8c9ede3a9a12d2e49d5567e79
is386 <is386@drexel.edu>
2022-01-28 22:15:32 -0500

    add cycles to main loop

--- i8080/machine.go
+++ i8080/machine.go
@@ -1,10 +1,30 @@
 package i8080
 
+import (
+	"time"
+
+	"github.com/veandco/go-sdl2/sdl"
+)
+
+var (
+	BUTTONS = map[sdl.Keycode]uint8{
+		sdl.K_SPACE:  0x1,
+		sdl.K_a:      0x20,
+		sdl.K_d:      0x40,
+		sdl.K_j:      0x10,
+		sdl.K_RETURN: 0x04,
+	}
+)
+
 type InvadersMachine struct {
-	cpu    *CPU
-	shift0 uint8
-	shift1 uint8
-	offset uint8
+	cpu           *CPU
+	shift0        uint8
+	shift1        uint8
+	offset        uint8
+	port1         uint8
+	interrupt     int
+	lastTimer     float64
+	nextInterrupt float64
 }
 
 func NewInvadersMachine(filename string, pcStart uint16, showDebug bool, isTest bool) *InvadersMachine {
@@ -20,10 +40,67 @@ func (im *InvadersMachine) setEmu(c *CPU) {
 }
 
 func (im *InvadersMachine) Run() {
+	if err := sdl.Init(sdl.INIT_EVERYTHING); err != nil {
+		panic(err)
+	}
+	defer sdl.Quit()
+
+	window, err := sdl.CreateWindow("Input", sdl.WINDOWPOS_UNDEFINED, sdl.WINDOWPOS_UNDEFINED, 800, 600, sdl.WINDOW_SHOWN)
+	if err != nil {
+		return
+	}
+	defer window.Destroy()
+
 	running := true
 	for running {
-		running = im.cpu.Execute()
+		now := getTime()
+
+		if im.lastTimer == 0 {
+			im.lastTimer = now
+			im.nextInterrupt = im.lastTimer + 16000.0
+			im.interrupt = 1
+		}
+
+		if im.cpu.intEnable == 1 && now > im.nextInterrupt {
+			if im.interrupt == 1 {
+				im.cpu.interrupt(1)
+				im.interrupt = 2
+			} else {
+				im.cpu.interrupt(2)
+				im.interrupt = 1
+			}
+			im.nextInterrupt = now + 8000.0
+		}
+
+		sinceLast := now - im.lastTimer
+		cyclesMax := 2 * sinceLast
+		cycles := 0.0
+		c := 0
+
+		for cyclesMax > cycles && running {
+			running, c = im.cpu.Execute()
+			cycles += float64(c)
+
+			for event := sdl.PollEvent(); event != nil; event = sdl.PollEvent() {
+				switch e := event.(type) {
+				case *sdl.QuitEvent:
+					running = false
+				case *sdl.KeyboardEvent:
+					switch e.Type {
+					case sdl.KEYDOWN:
+						im.keyDown(e.Keysym.Sym)
+					case sdl.KEYUP:
+						im.keyUp(e.Keysym.Sym)
+					}
+				}
+			}
+		}
+		im.lastTimer = now
 	}
+}
+
+func getTime() float64 {
+	return float64(time.Now().UnixNano()) / (float64(time.Millisecond) / float64(time.Nanosecond))
 }
 
 func (im *InvadersMachine) fetchNext() uint8 {
@@ -41,6 +118,10 @@ func (im *InvadersMachine) getAccumulator() uint8 {
 func (im *InvadersMachine) PortIn() {
 	port := im.fetchNext()
 	switch port {
+	case 0:
+		im.setAccumulator(1)
+	case 1:
+		im.setAccumulator(0)
 	case 3:
 		v16 := (uint16(im.shift1) << 8) | uint16(im.shift0)
 		v8 := uint8((v16 >> (8 - im.offset)) & 0xff)
@@ -58,4 +139,12 @@ func (im *InvadersMachine) PortOut() {
 		im.shift0 = im.shift1
 		im.shift1 = a
 	}
+}
+
+func (im *InvadersMachine) keyDown(key sdl.Keycode) {
+	im.port1 |= BUTTONS[key]
+}
+
+func (im *InvadersMachine) keyUp(key sdl.Keycode) {
+	im.port1 &= ^BUTTONS[key]
 }
